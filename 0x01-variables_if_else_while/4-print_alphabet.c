3ef2bc6df9843b48a9430f851bdae0196fb496ca
Tafadzwa Mukwepa <tbmukwepa@gmail.com>
2022-03-11 05:49:56 -0800

    print alphabet then new line

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#inlcude <stdio.h>
+/**
+ * main - main function
+ * Description: Prints out alphabet in lower case then new line
+ * Return: 0
+ */
+int main (void)
+{
+	car c;
+
+	for (c = 'a'; c<= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+	}
+	putchar(10);
+	return (0);
+}
