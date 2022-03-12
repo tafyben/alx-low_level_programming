294cb45ddaadf242817bc5b06dc426ea61b0a06f
Tafadzwa Mukwepa <tbmukwepa@gmail.com>
2022-03-11 21:24:19 -0800

    print numbers corrected rename

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Description: Prints numbers followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
