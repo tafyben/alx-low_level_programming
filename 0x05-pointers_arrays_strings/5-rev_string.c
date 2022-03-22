539563a004f0d61e0312c14c0d66fc72937472d3
Tafadzwa Mukwepa <tbmukwepa@gmail.com>
2022-03-22 15:10:58 +0200

    function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * rev_string - prints reversed string, followed by a new line
- * @s: pointer to the string to print
+ * @s: pointer to the string to print.
  * Return: void
 */
 
